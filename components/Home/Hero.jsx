f7d9766edfad7884d19f9f36032dc16abcdf3f30
Priyanshu Chahar <165533860+Priyannxhuu@users.noreply.github.com>
2024-06-05 15:14:26 +0000

    NodeElites

--- components/Home/Hero.jsx
+++ components/Home/Hero.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Syne } from "next/font/google";
+import { FaArrowRight } from "react-icons/fa6";
+import Link from "next/link";
 
 const syne = Syne({ subsets: ["latin"] });
 
@@ -10,13 +12,14 @@ const Hero = () => {
 
   return (
     <main className="flex justify-center items-center w-full">
-      <div className="flex flex-col justify-center items-center gap-1 px-2 lg:py-46 py-36 w-[95vw] lg:w-[80vw]">
-        <h1 className="text-5xl font-bold text-slate-200 text-center heading">
+      <div className="flex flex-col justify-center items-center gap-1 px-2 py-20 w-[95vw] lg:w-[80vw]">
+        <h1 className="text-4xl font-bold text-slate-200 text-center heading self-start">
           <p className={syne.className}>{heading}</p>
         </h1>
-        <h2 className="text-xl font-medium text-slate-300 text-center lg:w-[55vw] w-[90vw]">
+        <h2 className="text-xl font-medium text-slate-300 lg:w-[55vw] w-[90vw] self-start">
           {desc}
         </h2>
+        <Link href="" className="px-4 py-2 rounded-md mt-2 bg-slate-100 hover:bg-slate-300 transition ease-in-out text-zinc-900 font-semibold flex gap-2 items-center self-start">Join Community <p><FaArrowRight /></p></Link>
       </div>
     </main>
   );
