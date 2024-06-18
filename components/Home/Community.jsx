68aa05292aff69f83125ed91f06ff04351a9ae5f
Priyanshu Chahar <165533860+Priyannxhuu@users.noreply.github.com>
2024-06-18 18:44:36 +0530

    NodeElites

--- components/Home/Community.jsx
+++ components/Home/Community.jsx
@@ -37,7 +37,7 @@ const Community = () => {
             <div className="flex flex-col justify-center items-center gap-3 px-2 mb-20 w-[95vw] lg:w-[80vw]">
                 <h1 className="flex gap-2 text-3xl font-semibold text-slate-200 self-start items-center">{heading}</h1>
                 <h1 className="font-medium text-xl lg:text-2xl text-slate-200 self-start">{desc1}</h1>
-                <div className="grid grid-cols-2 md:grid-cols-3 justify-center items-stretch gap-5 mt-2 w-full">
+                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-center items-stretch gap-5 mt-2 w-full">
                 {communities.map((communitie , index)=>(
                <div key={index} className="flex flex-col text-center justify-center items-center gap-2 p-5 rounded-lg wrapper">
                 <h1 className="text-2xl text-slate-100 font-semibold">{communitie.name}</h1>
