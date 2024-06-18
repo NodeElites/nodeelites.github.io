cdf3b1e9340176e5503b87da11b9b227b160c522
Priyanshu Chahar <165533860+Priyannxhuu@users.noreply.github.com>
2024-06-18 12:06:38 +0000

    NodeElites

--- components/Home/PContribute.jsx
+++ components/Home/PContribute.jsx
@@ -34,7 +34,7 @@ const PContribute = () => {
                 <h1 className="font-medium text-xl lg:text-2xl text-slate-200 self-start">{desc1}</h1>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center items-stretch gap-5 mt-2 w-full">
                     {projexts.map((projext, index) => (
-                        <div key={index} className="flex flex-col justify-center items-center gap-2 rounded-lg wrapper">
+                        <div key={index} className="flex flex-col justify-center items-center gap-2 rounded-lg overflow-hidden wrapper">
                             <img src={projext.image} alt="" className="h-[125px] w-full object-cover"/>
                             <div className="flex flex-col justify-start items-center w-full gap-2 p-5">
                                 <h1 className="text-2xl pb-3 text-slate-100 font-semibold proj self-start">{projext.name}</h1>
